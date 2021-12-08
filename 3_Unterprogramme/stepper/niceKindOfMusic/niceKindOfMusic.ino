4f2dff5a5a50b4c314337f4c59987db537dce775
banltja <77324392+banltja@users.noreply.github.com>
2021-12-08 19:58:29 +0100

    changes

--- 3_Unterprogramme/stepper/niceKindOfMusic/niceKindOfMusic.ino
+++ 3_Unterprogramme/stepper/niceKindOfMusic/niceKindOfMusic.ino
@@ -3,7 +3,7 @@ const int stepPin = 1;
 const int stepsPerRevolution = 200;
 const int M0 = 10;
 const int M1 = 11;
-const int mode = 0;
+const int mode = 1;
 const int t = 350;
 
 
@@ -25,39 +25,85 @@ void setup() {
 }
 
 void loop() {
+  continuos(378, 100); //c
+  delay(100);
 
-  for (int i = 0; i <= 4; i++) {
-    turn(t*i);
-  }
 
-   for (int i = 4; i >= 0; i--) {
-    turn(t*i);
-  }
 
+  continuos(227, 100); //g
+  delay(100);
+  continuos(200, 100); //a
+  delay(100);
+
+  continuos(189, 100); //h
+  delay(100);
+
+  continuos(250, 100); //f
+  delay(100);
+
+  continuos(250, 100); //f
+  delay(100);
+  continuos(337, 300); //d
+  delay(100);
+  continuos(300, 100); //e
+  delay(100);
+  continuos(189, 100); //h
+  delay(100);
+
+  continuos(250, 50); //f
+  delay(100);
+  continuos(250, 100); //f
+  delay(100);
+  continuos(337, 100); //d
+  delay(100);
+  continuos(227, 200); //g
+  delay(100);
+  continuos(250, 250); //f
+  delay(100);
+  continuos(337, 100); //d
+  delay(100);
+
+  continuos(378, 100); //c
+  delay(100);
+  continuos(337, 100); //d
+  delay(100);
+
+  continuos(200, 500); //a
+  delay(100);
 }
 
-void continuos(int t) {
-  digitalWrite(stepPin, HIGH);
-  delayMicroseconds(toT(t));
-  digitalWrite(stepPin, LOW);
-  delayMicroseconds(toT(t));
-}
-
-void turn(int t) {
-  for (int i = 0; i <= stepsPerRevolution; i++) {
+void continuos(double t, int n) {
+  int i = 0;
+  while ( i < n) {
     digitalWrite(stepPin, HIGH);
     delayMicroseconds(toT(t));
     digitalWrite(stepPin, LOW);
     delayMicroseconds(toT(t));
+    i++;
   }
 
 }
+
 
 
 int toT(int t) {
   int rpm;
   rpm = t;
   return rpm;
+}
+
+void turn (int f, int d) {
+  double T = 1.0 / f;
+  int n = d / T;
+  int i = 0;
+  while ( i < n) {
+    digitalWrite(stepPin, HIGH);
+    delayMicroseconds(T);
+    digitalWrite(stepPin, LOW);
+    delayMicroseconds(T);
+
+    i++;
+  }
 }
 
 void setMode(int mode) {
