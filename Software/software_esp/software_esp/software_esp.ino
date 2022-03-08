f0071e3b732f1ff4ba9ef5a6d68b908c27ab0cc6
Purdbull <72924017+Purdbull@users.noreply.github.com>
2022-03-08 15:15:47 +0100

    Update software_esp.ino

--- Software/software_esp/software_esp/software_esp.ino
+++ Software/software_esp/software_esp/software_esp.ino
@@ -7,10 +7,16 @@ StateMashine handler;
 void setup() {
   Serial.begin(9600);
   pinMode(LED, OUTPUT);
-  handler.handle(B10001100);
+  handler.handle(B10001100); //Akkustand mit richtigem Tail
+  handler.handle(B00001100); //Akkustand mit falschem Tail
+  
   handler.handle("hi");
   handler.handle(B00010010);
-  handler.handle(B00001011);
+  handler.handle(B00000111); //akzeptierter fahrbefehl
+
+  handler.handle("hi");
+  handler.handle(B00011010);
+  handler.handle(B00001011); //abgelehnter fahrbefehl
 }
 void loop() {
   
