245df765d26fac6ab0aaf5f0edef13ae023edd71
banltja <77324392+banltja@users.noreply.github.com>
2022-03-23 13:45:56 +0100

    stopModes implemented

--- 3_Unterprogramme/stepper/MAIN/HighTrainStepper_V3/HighTrainStepper_V3.ino
+++ 3_Unterprogramme/stepper/MAIN/HighTrainStepper_V3/HighTrainStepper_V3.ino
@@ -6,9 +6,10 @@
 // PinOut
 #define dir 3
 #define clk 2
-#define en 10
+#define enbl 10
 #define m0 4
 #define m1 5
+#define led 9
 
 // Min and Max values for timer compare Register OCR1A
 #define Max  5500
@@ -16,10 +17,10 @@
 
 //Min and Max Values for acceleration (additional prescaler with modulo)
 #define aMax 20
-#define aMin 5
+#define aMin 4
 
 int y = Max; //target speed
-unsigned long k; //counter for ISR Prescaler, Iportant: LONG (!), overflow causes acc. stop
+int k; //counter for ISR Prescaler, Iportant: LONG (!), overflow causes acc. stop
 int a; //accelecaration (modulo)
 bool yDone = false;
 int s;
@@ -29,9 +30,10 @@ void setup() {
   Serial.begin(9600);
   pinMode(clk, OUTPUT);
   pinMode(dir, OUTPUT);
-  pinMode(en, OUTPUT);
+  pinMode(enbl, OUTPUT);
   pinMode(m0, OUTPUT);
   pinMode(m1, OUTPUT);
+  pinMode(led, OUTPUT);
   initInterrupts();
   linear(100);
 
@@ -39,28 +41,26 @@ void setup() {
 
 //-------------main--------------
 void loop() {
-  linear(100);
-  linear(100);
+  linear(50, 70);
+  delay(6000);
 
+  mode(1);
+  delay(5000);
+
+  linear(100, 100);
+  delay(5000);
+
+  mode(2);
   delay(8000);
-  linear(50);
-  linear(50);
-  linear(50);
+  linear(40);
+  delay(2000);
+  mode(3);
+  delay(6000);
+  mode(4);
   delay(8000);
 
-  /*
-    linear(10);
-    delay(6000);
 
 
-
-
-    linear(100,20);
-    delay(5000);
-
-    linear(20);
-    delay(5000);
-  */
 
 
 }
@@ -69,10 +69,10 @@ void loop() {
 //------------functions----------
 
 
-void linear(int s, int b)
-//s = newSpeed, b = acceleration
+void linear(int s, int b) {
+  //s = newSpeed, b = acceleration
 
-{
+  mode(0); //enable motor
 
   if (y != s) {
     y = map(s, 0, 100, Max, Min);
@@ -88,10 +88,10 @@ void linear(int s, int b)
 
 }
 
-void linear(int s)
-//s = newSpeed, b = acceleration
+void linear(int s) {
+  //s = newSpeed, b = acceleration
 
-{
+  mode(0); //enable motor
 
   if (y != s) {
 
@@ -100,13 +100,72 @@ void linear(int s)
   }
 
   if (a != 10) {
-    a = map(50, 0, 100, aMax, aMin);
+    a = map(40, 0, 100, aMax, aMin);
   }
-
 
 
 }
 
+
+void mode(int n) {
+  //idleMode
+  if (n == 0) {
+    en();
+    stepMode(1);
+  }
+
+  //emergencyStopMode
+  if (n == 1) {
+    Serial.println("---->EmergencyStop<----");
+    linear(0, 90);
+    delay(1);
+    while (!yDone) {
+      delay(1);
+    }
+    dis();
+  }
+
+  //stopMode
+  if (n == 2) {
+    Serial.println("---->Stop<----");
+    linear(0);
+    delay(1);
+    while (!yDone) {
+      delay(1);
+    }
+    stepMode(2);
+    delay(2000);
+    stepMode(4);
+    delay(1500);
+    dis();
+  }
+
+  // stopInStationMode
+  if (n == 3) {
+    Serial.println("---->StopInStation<----");
+    linear(0);
+    delay(1);
+    while (!yDone) {
+      delay(1);
+    }
+    stepMode(4);
+  }
+
+  // disableMotor
+
+  if (n == 4) {
+    dis();
+  }
+}
+
+void en() {
+  digitalWrite(enbl, 1);
+}
+
+void dis() {
+  digitalWrite(enbl, 0);
+  Serial.println("disabled");
+}
 
 void initInterrupts() {
   cli();                                //clear local interrupt flag
@@ -122,40 +181,27 @@ void initInterrupts() {
 
 }
 
-void setMode(int mode) {
+void stepMode(int mode) {
   //keep in mind: enable negative logic!
   //full step
   if (mode == 1) {
-    digitalWrite(en, 0);
     digitalWrite(m0, 0);
     digitalWrite(m1, 0);
   }
 
   //half step
   else if (mode == 2) {
-    digitalWrite(en, 0);
     digitalWrite(m0, 1);
     digitalWrite(m1, 0);
+    Serial.println("halfStep");
   }
 
   //micro step
   else if (mode == 4) {
-    digitalWrite(en, 0);
     digitalWrite(m0, 0);
     digitalWrite(m1, 1);
-  }
+    Serial.println("quarterStep");
 
-  //disable the motor
-  else if (mode == 0) {
-    digitalWrite(en, 1);
-  }
-
-  if (mode != 0) {
-    Serial.println("Mode " + String(mode) + " ativated");
-  }
-
-  else {
-    Serial.println("------------Motor disabled----------");
   }
 
 }
@@ -187,28 +233,40 @@ ISR(TIMER1_COMPA_vect) { //Timer1 Interrupt Service Routine
   digitalWrite(clk, !(digitalRead(clk)));
   k++;
 
-  if (OCR1A > y && k % a == 1) {
+  if (OCR1A > y && k >= a) {
+    //accelerate
+
     cli();
     OCR1A = OCR1A - 10;
     sei();
-    Serial.println(k);
+    k = 0;
 
   }
 
-  if (OCR1A < y && k % a == 1) {
+  if (OCR1A < y && k >= a) {
+    //decelerate
+
     cli();
     OCR1A = OCR1A + 10;
     sei();
-    Serial.println(k);
+    k = 0;
 
 
   }
 
-  
-    if (abs(OCR1A - y) <= 10) {
-      OCR1A = y;
-    }
-  
+
+  if (abs(OCR1A - y) <= 10) {
+    OCR1A = y;
+    yDone = true;
+    digitalWrite(led, 1);
+  }
+
+  else {
+    yDone = false;
+    digitalWrite(led, 0);
+  }
+
+
 
 
 }
