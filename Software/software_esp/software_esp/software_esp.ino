5cc418b13923b65790526cc2e6df430f920f0457
Purdbull <72924017+Purdbull@users.noreply.github.com>
2022-04-28 20:19:52 +0200

    prepared for esp stuff

--- Software/software_esp/software_esp/software_esp.ino
+++ Software/software_esp/software_esp/software_esp.ino
@@ -1,19 +1,86 @@
 #include "StateMashine.h"
-//00#include "EspMQTTClient.h"
+#include <WiFi.h>
+#include <PubSubClient.h>
 #include "EEPROM.h"
 
 #define REED_PIN 4
 
 bool isDrivingForward = true;
 
-/*EspMQTTClient client(
-  "WifiSSID",
-  "WifiPassword",
-  "192.168.1.100",  // MQTT Broker server ip
-  "TestClient"      // Client name that uniquely identify your device
-);*/
 
-//EspMQTTClient* clientPtr = 0; //&client;
+//################################################
+//##           Wifi and MQTT Stuff              ##
+//################################################
+const char* ssid = "........";
+const char* password = "........";
+const char* mqtt_server = "broker.mqtt-dashboard.com";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+unsigned long lastMsg = 0;
+#define MSG_BUFFER_SIZE  (50)
+char msg[MSG_BUFFER_SIZE];
+int value = 0;
+
+void setup_wifi() {
+
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  randomSeed(micros());
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish("outTopic", "hello world");
+      // ... and resubscribe
+      client.subscribe("inTopic");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+
 StateMashine* handler = new StateMashine();
 
 void IRAM_ATTR ISR() {
@@ -38,21 +105,12 @@ void setup() {
   //attachInterrupt(REED_PIN, ISR, RISING);
   EEPROM.write(42, B00000011); //Fake position 3 for testing
   pinMode(2, OUTPUT);
+
+  
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
 }
-
-
-
-/*void onConnectionEstablished()
-{
-  // Subscribe to "mytopic/test" and display received message to Serial
-  client.subscribe("train/driveToPosition", [](const String & payload) {
-    handler->handle(payload);
-    isDrivingForward = (payload.toInt() > EEPROM.read(42));
-  });
-
-  // Publish a message to "mytopic/test"
-  client.publish("train/position", "This is a message"); // You can activate the retain flag by setting the third parameter to true
-}*/
 
 void clearSerialBuffer() {
   char c;
@@ -62,15 +120,14 @@ void clearSerialBuffer() {
 }
 
 long mils = 0;
+
 void loop() {
   
   handler->handle("3");
-  mils = millis();
-  while(millis()-mils<20000){
-  //client.loop(); testing
+  client.loop();
   if (Serial.available() > 0) {
     handler->handle(Serial.read());
     clearSerialBuffer();
   }
-  }
+  
 }
